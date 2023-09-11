4ae1f1f725d326bb5c1d8884641014e9ec0ea6a0
Esma Beydili <106523107+Esma222@users.noreply.github.com>
2023-09-11 11:39:54 +0300

    Update ArrayStreamExample.java

--- StreamExamples/src/ArrayStreamExample.java
+++ StreamExamples/src/ArrayStreamExample.java
@@ -4,7 +4,7 @@ public class ArrayStreamExample {
     public static void main(String[] args) {
         int[] numbers = {1,2,3,3,5,6,6,8,0};
         System.out.println(Arrays.stream(numbers).anyMatch(number-> number<=4));
-Arrays.stream(numbers).
+
 
     }
 }
